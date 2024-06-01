d1dca9be60ccb0385c7fc2efe9b4f8462fb978da
Sekou Diane <sekoudiane1990@gmail.com>
2024-06-01 12:07:38 +0300

    PRIS: pr20 tanks fixed moving and shooting

--- PRIS/pr20/main.py
+++ PRIS/pr20/main.py
@@ -60,6 +60,27 @@ class Bullet:
         self.x+=vec[0]*dt
         self.y+=vec[1]*dt
 
+class Explosion:
+    def __init__(self, x, y, rMax):
+        self.x=x
+        self.y=y
+        self.rMax=rMax
+        self.r=0
+        self.exploded = False
+
+    def getPos(self):
+        return [self.x, self.y]
+
+    def sim(self, dt):
+        if not self.exploded:
+            self.r+=50*dt
+            if self.r>self.rMax:
+                self.exploded=True
+
+    def draw(self, screen):
+        w=5*(1-self.r/self.rMax)
+        w=max(1, round(w))
+        pygame.draw.circle(screen, (200,200,0), self.getPos(), self.r, w)
 class Tank:
     def __init__(self, id, x, y, ang):
         self.id=id
@@ -74,13 +95,34 @@ class Tank:
         self.va=0
         self.vaGun=0
         self.health=100
+        self.fireDelay=1
+        self.lastFireDelay=0
     def fire(self):
+        self.lastFireDelay=0
         r = self.W / 2.3
         LGun = self.L / 2
         p2 = rot([r + LGun, 0], self.ang + self.angGun)
         p2=np.add(self.getPos(), p2)
         b=Bullet(*p2, self.ang + self.angGun)
         return b
+    def control(self, tanks, bullets):
+        tanks_=[t for t in tanks if t!=self]
+        if len(tanks_)==0:
+            return
+        dd=[dist(t.getPos(), self.getPos()) for t in tanks_]
+        i = np.argmin(dd)
+        target=tanks_[i]
+        dx=target.x-self.x
+        dy=target.y-self.y
+        angTarget=math.atan2(dy, dx)
+        daGun=limAng(angTarget-(self.ang+self.angGun))
+        self.vaGun=15*daGun
+        if self.lastFireDelay>=self.fireDelay and abs(daGun)<0.1:
+            bullets.append(self.fire())
+
+        da=limAng(angTarget-self.ang)
+        self.va=1*da
+
     def getPos(self):
         return [self.x, self.y]
     def draw(self, screen):
@@ -88,6 +130,8 @@ class Tank:
         pts=rotArr(pts, self.ang)
         pts=np.add(pts, self.getPos())
         pygame.draw.polygon(screen, (0,0,0), pts, 2)
+        if self.health<=0:
+            pygame.draw.polygon(screen, (0, 0, 0), pts, 0)
         r=self.W/2.3
         pygame.draw.circle(screen, (0,0,0), self.getPos(), r, 2)
         LGun=self.L/2
@@ -96,30 +140,60 @@ class Tank:
         p2=rot([r+LGun, 0], self.ang+self.angGun)
         pygame.draw.line(screen, (0,0,0), np.add(p0, p1), np.add(p0, p2), 3)
         drawText(screen, f"{self.id} ({self.health})", self.x, self.y - self.L/2 - 12)
-    def sim(self, dt):
+    def sim(self, dt, allTanks):
+        if self.health<=0:
+            self.vx=self.vy=self.va=self.vaGun=0
         vec=[self.vx, self.vy]
         vec=rot(vec, self.ang)
         self.x+=vec[0]*dt
         self.y+=vec[1]*dt
         self.ang+=self.va*dt
         self.angGun+=self.vaGun*dt
+        self.lastFireDelay+=dt
+
+        vecMin=np.zeros(2)
+        LMin=100500
+        tanks_=[t for t in allTanks if t!=self]
+        for t in tanks_:
+            vec=np.subtract(self.getPos(), t.getPos())
+            L=np.linalg.norm(vec)
+            if L<LMin:
+                vecMin=vec
+                LMin=L
+        if LMin<self.L:
+            vecMin=20*vecMin/np.linalg.norm(vecMin)
+            self.x+=vecMin[0]*dt
+            self.y+=vecMin[1]*dt
+
+
+#class Explosion
+#obstacles
+#tank obstacles
+#чет столкновений для потери здоровья танков
+
 
 def main():
     screen = pygame.display.set_mode(sz)
     timer = pygame.time.Clock()
     fps = 20
-    tank=Tank(0, 200, 200, 1)
-    tank.vx=20
-    tank.va=-1
-    tank.vaGun=-0.5
-    tank1=Tank(0, 400, 300, 1)
-    tank1.vx=20
-    tank1.va=1
-    tank1.vaGun=-0.5
 
-    tanks=[tank, tank1]
+    np.random.seed(0)
+
+    tanks=[]
+    tanksExploded=[]
+
+
+    for i in range(8):
+        tank = Tank(i, np.random.randint(50, 750),
+                    np.random.randint(50, 550),
+                    np.random.random()*math.pi*2)
+        tank.vx = 20
+        tank.va = -1
+        tank.vaGun = -0.5
+        tanks.append(tank)
 
     bullets=[]
+    explosions=[]
 
     while True:
         for ev in pygame.event.get():
@@ -127,33 +201,49 @@ def main():
                 sys.exit(0)
             if ev.type == pygame.KEYDOWN:
                 if ev.key == pygame.K_1:
-                    b=tank.fire()
+                    b=tanks[0].fire()
                     bullets.append(b)
 
         dt=1/fps
+        for e in explosions:
+            e.sim(dt)
+        explosions = [e for e in explosions if not e.exploded]
+
         for t in tanks:
-            t.sim(dt)
+            t.control(tanks, bullets)
+        for t in tanks:
+            t.sim(dt, [*tanks, *tanksExploded])
         for b in bullets:
             b.sim(dt)
             if dist(b.getPos(), [sz[0] / 2, sz[1] / 2]) > sz[0]:
                 b.exploded=True
             for t in tanks:
-                if dist(t.getPos(), b.getPos())<t.L/2:
+                if dist(t.getPos(), b.getPos())<t.W/2:
                     b.exploded = True
-                    t.health-=10
+                    t.health=max(0, t.health-10)
+                    expl=Explosion(*b.getPos(), 35)
+                    explosions.append(expl)
                     break
 
         bullets=[b for b in bullets if not b.exploded]
 
+        #перенос подбитых танков
+        tanksExploded.extend([t for t in tanks if t.health<=0])
+        tanks=[t for t in tanks if t.health>0]
+
 
         screen.fill((255, 255, 255))
-        tank.draw(screen)
+        # tank.draw(screen)
 
 
         for b in bullets:
             b.draw(screen)
         for t in tanks:
             t.draw(screen)
+        for t in tanksExploded:
+            t.draw(screen)
+        for e in explosions:
+            e.draw(screen)
 
         drawText(screen, f"NBullets = {len(bullets)}", 5, 5)
 
